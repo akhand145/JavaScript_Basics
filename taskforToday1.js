d922ff054ac7895f5e244913cbb69dc594ef93b9
akhand145 <54594782+akhand145@users.noreply.github.com>
2022-06-01 18:58:09 +0530

    with the help of while loop

---
+++ taskforToday1.js
@@ -0,0 +1,30 @@
+//Given an array of integers, keep a total score based on the following:
+//1. Add 1 point for every even number in the array
+//2. Add 3 points for every odd number in the array, except for the number "5"
+//3. Add 5 points every time the number "5" appears in the array
+//4. Add 1 point for every 0 in  the array
+
+//Input: [1, 2, 3, 4, 5]  Output: 13
+//Input: [17, 19, 21] Output: 9
+//Input: [5, 5, 5] Output: 15
+
+
+const arr1 = [1, 2, 3, 4, 5];
+// const arr1 = [17, 19, 21];
+// const arr1 = [5, 5, 5];
+let sum = 0;
+let  i =0; 
+
+while(i<arr1.length){
+    if((arr1[i] % 2 != 0) && (arr1[i] != 5)){
+        sum += 3;
+    }
+    else if((arr1[i] % 2 == 0) || (arr1[i] == 0)){
+        sum += 1;
+    }
+    else if(arr1[i] == 5){
+        sum += 5;
+    }
+    i++
+}
+console.log(sum);
