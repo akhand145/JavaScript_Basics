7fb432675bd38b50f2754476099c38901b0c164d
akhand145 <54594782+akhand145@users.noreply.github.com>
2022-06-01 13:37:51 +0530

    Example of Comparison operators

---
+++ comparisons.js
@@ -0,0 +1,13 @@
+console.log(5>4);  // true, boolean logical operator
+
+console.log("apple" > "pineapple");  // false, string operator
+
+console.log("2" > "12");  // false, logical operator
+
+console.log(undefined == null);  // true, Null & undefined equals 
+
+console.log(undefined === null);  // false, it checks type string also
+
+console.log(null == "\n0\n");  // false, null only equals undefined
+
+console.log(null === +"\n0\n");  // false, it checks type string also
