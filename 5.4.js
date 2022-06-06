30749d8926625b4b102e720ea28a7f8be60da99f
Your Name <you@example.com>
2022-06-06 17:35:53 +0530

    Added my project

---
+++ 5.4.js
@@ -0,0 +1,56 @@
+///////////////////// Arrays ///////////////////////////
+// Object allows us to create Keys value of Collection
+// Array holds the similar types of data elements
+
+///////////// Array Declaration ///////////////
+
+let fruits = ["Apple", "Mango", "Banana"];
+
+console.log( fruits[0] );  // Apple
+console.log( fruits[1] );  // Mango
+console.log( fruits[2] );  // Banana
+
+///// Total count of elements in the array : length
+console.log( fruits.length );  // 3
+
+// Calculate the last elements index : fruits[fruits.length - 1]
+console.log( fruits[fruits.length -1 ] );
+
+// shorter syntax: fruits.at(-1)
+console.log( fruits.at(-1) );
+
+// Push : Adds an element to the end.
+// Pop : Takes an element from the end. 
+
+// Arrays in JS can work both as a Stack and Queue
+// Stack follows LIFO (Last-In-First-Out)
+// Queue follows FIFO (First-In-First-Out)
+
+// Example of pop()
+fruits.pop();
+console.log( fruits );
+
+// Example of push()
+fruits.push("Banana");
+console.log( fruits );
+
+// Shift and unshift:
+fruits.shift();
+console.log( fruits );
+
+fruits.unshift("Apple");
+console.log( fruits );
+
+// Method Push and Unshift can add multiple elements:
+fruits.push("Orange", "Lemon");
+fruits.unshift("Pineapple", "Peach");
+
+console.log( fruits );
+
+// Internals : arr[0]
+let arr = fruits;
+console.log( arr === fruits );
+arr.push("Pear");
+console.log( fruits );
+
+//////////////////////////////////////////////////////////////
