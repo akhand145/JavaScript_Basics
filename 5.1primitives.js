f1a4eff840f9493cb95cc7e2f1ec22f55c57e491
akhand145 <54594782+akhand145@users.noreply.github.com>
2022-06-11 22:57:53 +0530

    Methods of Primitives

---
+++ 5.1primitives.js
@@ -0,0 +1,24 @@
+////////// Methods  of Primitives ////////////
+
+let rohit = {
+    name: "Shubhm",
+    sayHi: function() {
+      console.log("Hi buddy!");
+    }
+  };
+  
+  rohit.sayHi(); // Hi buddy!s
+
+////// A primitive as an object str.toUpperCase() //////
+
+  let str = "Hello";
+
+  console.log( str.toUpperCase() ); // HELLO
+
+///////////////////// to.Fixed(n) ////////////////////////
+
+let n = 1.23456;
+
+console.log( n.toFixed(2) ); // 1.23
+
+//////////////////////////////////////////
