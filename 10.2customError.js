b2d09ef59ee9621f49a52f16c720837dea416891
Akhand <akhandsingh145@gmail.com>
2022-06-16 10:24:52 +0530

    Custom error & Extending error

---
+++ 10.2customError.js
@@ -0,0 +1,20 @@
+////////////////// Custom Errors, Extending Error ///////////////////
+
+// Task: 1.
+
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+let err = new FormatError("formatting error");
+
+console.log( err.message ); // formatting error
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
+
+console.log( err instanceof SyntaxError ); // true
+
+/////////////////////////////////////////////////////////
