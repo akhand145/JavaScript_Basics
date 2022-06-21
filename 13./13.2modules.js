ea76deec1efd3b1c34da581cc379befa0eb0b9c1
Akhand <akhandsingh145@gmail.com>
2022-06-21 17:51:40 +0530

    Modules Export/Import

---
+++ 13./13.2modules.js
@@ -0,0 +1,24 @@
+////////////////////// Modules ////////////////////////
+
+// Modules can load each other and use special directives export and import to interchange
+// functionality, call functions of one module from another one:
+
+// export keyword labels variables and functions that should be accessible from outside the 
+// current module.
+// import allows the import of functionality from other modules.
+
+
+// export a single value, without making a variable
+// export default ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+// export default class User {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// export function sayHi(user) {
+//   console.log(`Hello, ${user}!`);
+// }
+
