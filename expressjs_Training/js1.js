5c8fc5eb3c2c0df28aa2ff2d3647d12b7b6adecf
Akhand <akhandsingh145@gmail.com>
2022-07-07 18:27:26 +0530

    Training

---
+++ expressjs_Training/js1.js
@@ -0,0 +1,26 @@
+const a = 7;
+const b = 8;
+
+// console.log(greaterOrNot(a, b));
+
+function greet(name, greetText) {
+
+    console.log(greetText)
+    // let name = "Name1";
+    console.log(name + " is a good boy ");
+}
+
+let name = "Harry";
+let name1 = "Rohan";
+let name2 = "Shubham";
+let name3 = "Sagar";
+
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
+
+// console.log(name + " " + "is a good boy");
+// console.log(name1 + " " + "is a good boy");
+// console.log(name2 + " " + "is a good boy");
+// console.log(name3 + " " + "is a good boy");
